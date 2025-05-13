3989a1501294b7eb5966e80be704aac8388d1928
zinsile <zinsileda@gmail.com>
2025-05-13 15:02:42 +0900

    feat : data modify

--- main.py
+++ main.py
@@ -6,22 +6,14 @@ import pandas as pd
 import numpy as np
 
 generate_df = pd.read_csv(GNERATE_DATA_SOURCE_PATH)
+
 generate_df.rename(columns={
     "tempMode": "temp_mode",
-    "Temperature": "temperature"
-}, inplace=True)
+    "Temperature": "temperature"}, inplace=True)
+
 # 누락된 컬럼 채우기
 if 'fail_probability' not in generate_df.columns:
     generate_df['fail_probability'] = np.nan
 
 for idx, row in generate_df.iterrows():
-    row_dict = row.to_dict()
-    
-    # Pydantic 모델로 변환
-    sensor_data = SensorDataCreate(**row_dict)
-    print(sensor_data)
-
-    # @app.on_event("startup")
-    # async def startup():
-    #     asyncio.create_task(periodic_task(predict_failures(), 5)) # 5초 마다 
-    #     asyncio.create_task(periodic_task(evaluate_and_retrain(), 86400)) # 24시간 마다
+    print(row)
